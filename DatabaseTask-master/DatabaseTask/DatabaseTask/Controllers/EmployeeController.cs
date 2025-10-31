3531c7c7a33517d8b3be567afa2599a45edd9b4f
opilane <opilane@HP-CZC0359NNC>
2025-10-31 16:31:01 +0200

    Stored procedure is not finish, i am cooked

---
+++ DatabaseTask-master/DatabaseTask/DatabaseTask/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using DatabaseTask.Core.Domain;
+using DatabaseTask.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace DatabaseTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public DatabaseTaskDbContext _context;
+        public IConfiguration _confiq;
+
+        public EmployeeController (DatabaseTaskDbContext context, IConfiguration confiq)
+        {
+            _context = context;
+            _confiq = confiq;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string connectionStr = _confiq.GetConnectionString("DefaultConnection");
+            
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "spSearchEmployee";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.JobTitle = sdr["JobTitle"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+
+                return View(model);
+            }
+        }
+    }
+}
--- DatabaseTask-master/DatabaseTask/DatabaseTask/Program.cs
+++ DatabaseTask-master/DatabaseTask/DatabaseTask/Program.cs
@@ -10,13 +10,6 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DatabaseTaskDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-//void ConfigureServices(IServiceCollection services)
-//{
-//    services.AddDbContext<DatabaseTaskDbContext>(options =>
-//        options.UseSqlServer(Microsoft.Extensions.Configuration.GetConnectionString("databasename")));
-//}
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
